d5c4ac9dcdd76e13ce4894ea7745765fc010a274
unknown <1215772764@qq.com>
2021-03-06 15:31:29 +0800

    :tada:init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'file-tools'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
